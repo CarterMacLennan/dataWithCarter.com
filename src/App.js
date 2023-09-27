de166464efef72f019fc402a831d0179071a9008
Carter MacLennan <cartermaclennan@cartermaclellan-mbp-904ll.home>
2023-09-27 19:53:32 -0300

    try removing the basename in router

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="/dataWithCarter">
+    <Router basename="/">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <NavBar/>
