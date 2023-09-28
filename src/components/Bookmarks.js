e12aa434a6117d0dd42dd873f1ca09453990818a
Carter MacLennan <cartermaclennan@cartermaclellan-mbp-904ll.home>
2023-09-27 21:21:36 -0300

    update bookmarks

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -4,17 +4,12 @@ import neetcode from "../Assets/Bookmarks/neetcode.png";
 import ByteByteGo from "../Assets/Bookmarks/byteByteGo.png";
 import exponent from "../Assets/Bookmarks/exponent.png";
 import narendraL from "../Assets/Bookmarks/narendraL.png";
-import seattleDataGuy from "../Assets/Bookmarks/seattleDataGuy.png";
-import kahan from "../Assets/Bookmarks/kahan.png";
 import mattWeingarten from "../Assets/Bookmarks/mattWeingarten.png";
-import { AiOutlineConsoleSql } from "react-icons/ai";
-import { SiLeetcode } from "react-icons/si";
-import {MdOutlineDesignServices} from "react-icons/md"
-import {FaAws} from "react-icons/fa"
 import ProjectCard from "./ProjectCards";
 import myArchitecture from "../Assets/Bookmarks/myArchitecture.png";
 import stratascratch from "../Assets/Bookmarks/stratascratch.png";
-import { HiOutlineDatabase } from "react-icons/hi";
+import freeCodeCamp from "../Assets/Bookmarks/freeCodeCamp.png";
+import gauravSen from "../Assets/Bookmarks/gauravSen.png";
 
 function Bookmarks() {
   return (
@@ -29,7 +24,6 @@ function Bookmarks() {
               <ProjectCard
                 imgPath={neetcode}
                 title={"NeetCode.io"}
-                icon={<SiLeetcode style={{ color: "white" }} />}
                 link={"https://neetcode.io/"}
               />
             </Col>
@@ -37,7 +31,6 @@ function Bookmarks() {
               <ProjectCard
                 imgPath={ByteByteGo}
                 title="ByteByteGo"
-                icon={<MdOutlineDesignServices style={{ color: "white" }} />}
                 link={"https://www.youtube.com/channel/UCZgt6AzoyjslHTC9dz0UoTw"}
               />
             </Col>
@@ -45,57 +38,49 @@ function Bookmarks() {
               <ProjectCard
                 imgPath={myArchitecture}
                 title="AWS Architecture Series"
-                icon={<FaAws style={{ color: "white" }} />}
                 link={"https://www.youtube.com/watch?v=H_S7CxtHgSM&list=PLhr1KZpdzukdeX8mQ2qO73bg6UKQHYsHb"}
-              />
-            </Col>
-
-            <Col md={9} lg={4} className="project-card center">
-              <ProjectCard
-                imgPath={narendraL}
-                title="NarendraL"
-                icon={<MdOutlineDesignServices style={{ color: "white" }} />}
-                link={"https://www.youtube.com/@TechDummiesNarendraL/videos"}
-              />
-            </Col>
-            <Col md={9} lg={4} className="project-card center">
-              <ProjectCard
-                imgPath={seattleDataGuy}
-                title="Seattle Data Guy"
-                icon={<HiOutlineDatabase style={{ color: "white" }} />}
-                link={"https://www.youtube.com/@SeattleDataGuy"}
-              />
-            </Col>
-             <Col md={9} lg={4} className="project-card center">
-              <ProjectCard
-                imgPath={mattWeingarten}
-                title="Matt Weingarten's Blogs"
-                icon={<HiOutlineDatabase style={{ color: "white" }} />}
-                link={"https://medium.com/@matt_weingarten"}
               />
             </Col>
             <Col md={9} lg={4} className="project-card center">
               <ProjectCard
                 imgPath={stratascratch}
                 title={"StrataScratch"}
-                icon={<AiOutlineConsoleSql style={{ color: "white" }} />}
                 link={"https://www.youtube.com/@stratascratch"}
-              />
-            </Col>
-            <Col md={9} lg={4} className="project-card center">
-              <ProjectCard
-                imgPath={kahan}
-                title="Kahan Data Solutions"
-                icon={<HiOutlineDatabase style={{ color: "white" }} />}
-                link={"https://www.youtube.com/@KahanDataSolutions/videos"}
               />
             </Col>
             <Col md={9} lg={4} className="project-card center">
               <ProjectCard
                 imgPath={exponent}
                 title={"Exponent"}
-                icon={<MdOutlineDesignServices style={{ color: "white" }} />}
                 link={"https://www.youtube.com/@tryexponent"}
+              />
+            </Col>
+            <Col md={9} lg={4} className="project-card center">
+              <ProjectCard
+                imgPath={freeCodeCamp}
+                title="Free Code Camp"
+                link={"https://www.youtube.com/@freecodecamp"}
+              />
+            </Col>
+             <Col md={9} lg={4} className="project-card center">
+              <ProjectCard
+                imgPath={mattWeingarten}
+                title="Matt Weingarten's Blogs"
+                link={"https://medium.com/@matt_weingarten"}
+              />
+            </Col>
+            <Col md={9} lg={4} className="project-card center">
+              <ProjectCard
+                imgPath={narendraL}
+                title="NarendraL"
+                link={"https://www.youtube.com/@TechDummiesNarendraL/videos"}
+              />
+            </Col>
+             <Col md={9} lg={4} className="project-card center">
+              <ProjectCard
+                imgPath={gauravSen}
+                title="Gaurav Sen"
+                link={"https://www.youtube.com/@gkcs"}
               />
             </Col>
           </Row>
